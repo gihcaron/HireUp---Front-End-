cbe4367de7e46113ab4218c45633bbf891b7062a
Giovanna Caron <giovanna.c.barros@aluno.senai.br>
2025-11-06 16:19:53 -0300

    Feat: create banner

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,11 +1,33 @@
 "use client";
 import React from "react";
+import Image from "next/image";
+import styles from "./styles.module.css";
 
 import Header from "../../Components/Header";
 export default function home() {
   return (
-    <section>
+    <section className={styles.homeContainer}>
       <Header />
+
+      <section className={styles.bannerSection}>
+        <h1 className={styles.titleBanner}>
+          Do primeiro contato à contratação,
+          <span className={styles.coloredTitle}>
+            {" "}
+            contrate melhor com o HireUp.
+          </span>
+        </h1>
+        <p className={styles.subtitleBanner}>
+          Centralize seu processo seletivo com eficiência e simplicidade.
+        </p>
+        <Image
+          src="/images/workGroup.png"
+          alt="Banner Image"
+          width={600}
+          height={400}
+          className={styles.bannerImage}
+        />
+      </section>
     </section>
   );
 }
