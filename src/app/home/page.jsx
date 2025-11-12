5d455ba1490369c7b59af610f7968eefc907dcce
Giovanna <giovannacaron.tds1.senai@gmail.com>
2025-11-12 20:46:15 -0300

    feat: create and style call-to-action section

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -33,27 +33,36 @@ export default function home() {
 
       </section>
       <section className={styles.statsSection}>
-      <div className={styles.statsContainer}>
-        <div className={styles.statCardBlue}>
-          <h2>50K</h2>
-          <p>candidatos</p>
-          <p>ativos</p>
+        <div className={styles.statsContainer}>
+          <div className={styles.statCardBlue}>
+            <h2>50K</h2>
+            <p>candidatos</p>
+            <p>ativos</p>
+          </div>
+          <div className={styles.statCard}>
+            <h2>13+</h2>
+            <p>empresas</p>
+          </div>
+          <div className={styles.statCard}>
+            <h2>20</h2>
+            <p>vagas abertas</p>
+          </div>
+          <div className={styles.statCard}>
+            <h2>10K</h2>
+            <p>entrevistas</p>
+          </div>
         </div>
-        <div className={styles.statCard}>
-          <h2>13+</h2>
-          <p>empresas</p>
-        </div>
-        <div className={styles.statCard}>
-          <h2>20</h2>
-          <p>vagas abertas</p>
-        </div>
-        <div className={styles.statCard}>
-          <h2>10K</h2>
-          <p>entrevistas</p>
-        </div>
-      </div>
       </section>
-
+        <section className={styles.callToActionSection}>
+          <div className={styles.callToActionContainer}>
+          <div className={styles.callToActionTitleArea} >
+          <h2 className={styles.callToActionTitle}>
+            Pronto para transformar seu processo de contratação?
+          </h2>
+         <p className={styles.callToActionDescription}> Na HireUp você combina as melhores vagas com seu perfil profissional. Está esperando o que para alavancar sua carreira, como já. Seja HireUP. </p>
+          </div>
+          </div>
+        </section>
     </section>
   );
 }
